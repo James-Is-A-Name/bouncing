a3d1be14bf45ec918ed494570e5288ca043a2067
James <42189301+James-Is-A-Name@users.noreply.github.com>
2018-10-11 22:09:03 +1300

    add movement of items

--- scripts/bounce.js
+++ scripts/bounce.js
@@ -7,27 +7,54 @@ var bounceItems = {
     height: 800
 }
 
+
+class bounceItem{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 function startBounce(){
     let area = document.getElementById("displayArea");
 
     area.style.height = bounceItems.height+"px";
     area.style.width = bounceItems.width+"px";
 
+    setupItems()
+
     drawItems();
-    moveItem();
+
+    moveItems();
 }
 
+function setupItems(){
 
-function drawItems(){
     let area = document.getElementById("displayArea");
 
-    area.innerHTML = "<div class=\"aBlob\" id=\"item1\">Test</div>";
+    //will want to change these numbers to less arbitrary
+    bounceItems.items.push(new bounceItem(10,10));
+
+    //this assignment is not the best
+    let indexId = bounceItems.items.length;
+
+    area.innerHTML = "<div class=\"aBlob\" id=\"item"+(indexId-1)+"\">Test</div>";
 }
 
-function moveItem(){
-    let theItem = document.getElementById("item1");
+function drawItems(){
 
-    theItem.style.left = "10px";
-    theItem.style.top = "10px";
+    //will take on some of what is being done in moveItems
+}
 
+function moveItems(){
+
+    let theItem = document.getElementById("item0");
+    
+    bounceItems.items[0].x += 1;
+    bounceItems.items[0].y += 1;
+
+    theItem.style.left = bounceItems.items[0].x + "px";
+    theItem.style.top = bounceItems.items[0].y + "px";
+
+    setTimeout("moveItems()",30);
 }
