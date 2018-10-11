eb32406242ceb347552938f358cc82eba4fae2ef
James <42189301+James-Is-A-Name@users.noreply.github.com>
2018-10-11 23:17:33 +1300

    add item add function. altered side bounce to accomodate item size

--- scripts/bounce.js
+++ scripts/bounce.js
@@ -3,18 +3,21 @@ document.addEventListener("DOMContentLoaded",startBounce);
 
 var bounceItems = {
     items : [],
-    width: 800,
-    height: 800
+    width: 400,
+    height: 400
 }
 
 
 class bounceItem{
-    constructor(x,y){
+    constructor(x,y,width,height,xMovement,yMovement){
         this.x = x;
         this.y = y;
 
-        this.xMovement = 1;
-        this.yMovement = 1;
+        this.width = width;
+        this.height = height;
+
+        this.xMovement = xMovement;
+        this.yMovement = yMovement;
     }
 }
 
@@ -35,24 +38,26 @@ function setupItems(){
 
     let area = document.getElementById("displayArea");
 
-    //TODO FIX HOW THIS IS DONE
-
     area.innerHTML = ""
 
+    addItem(new bounceItem(10,10, 40,40, 1,1));
+    addItem(new bounceItem(60,100, 50,50, 1,-1));
+}
+function addItem(newItem){
+
+    //TODO fix this 
+
     //will want to change these numbers to less arbitrary
-    bounceItems.items.push(new bounceItem(10,10));
+    bounceItems.items.push(newItem);
 
     //this assignment is not the best
     let indexId = bounceItems.items.length - 1;
-
-    //will want to setup a better way then just lineing up the array with the elements
-    area.innerHTML += "<div class=\"aBlob\" value=\""+indexId+"\" id=\"item"+indexId+"\">Test</div>";
-
     
-    //repeat of the above for inital testing
-    bounceItems.items.push(new bounceItem(50,50));
-    indexId = bounceItems.items.length - 1;
+    //will want to setup a better way then just lineing up the array with the elements
+        //for one probably will want to change to using create element and append functions
+    let area = document.getElementById("displayArea");
     area.innerHTML += "<div class=\"aBlob\" value=\""+indexId+"\" id=\"item"+indexId+"\">Test</div>";
+
 }
 
 function drawItems(){
@@ -66,6 +71,10 @@ function drawItems(){
 
         htmlItem.style.left = bounceItems.items[index].x + "px";
         htmlItem.style.top = bounceItems.items[index].y + "px";
+
+        //This might be being over called. wont change each movement
+        htmlItem.style.width = bounceItems.items[index].width + "px";
+        htmlItem.style.height = bounceItems.items[index].height + "px";
     });
 }
 
@@ -73,14 +82,14 @@ function moveItems(){
     
     bounceItems.items.forEach((item) => {
 
-        if(item.x > bounceItems.width){
+        if(item.x > (bounceItems.width - item.width)){
             item.xMovement = -1;
         }
         else if(item.x < 0){
             item.xMovement = 1;
         }
         
-        if(item.y > bounceItems.height){
+        if(item.y > (bounceItems.height - item.height)){
             item.yMovement = -1;
         }
         else if(item.y < 0){
